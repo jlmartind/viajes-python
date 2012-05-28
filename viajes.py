13723859a22f29f745239272bbf4d1caf724fa6f
José Luis Martín <jlmartind@hotmail.com>
2012-05-28 23:27:20 +0200

    viajes.py modificado envio, ahora envía XML, pero responde NO AVAILABILITY

--- viajes.py
+++ viajes.py
@@ -1,13 +1,42 @@
 #!/usr/bin/python
 from lxml import etree
-import httplib,urllib,sys
+import urllib2,sys,urllib
 import cgi
 import cgitb
+
+
 cgitb.enable()
 print "Content-Type: text/html"     # HTML is following
 print                               # blank line, end of headers
 
 form = cgi.FieldStorage()
+
+
+# Lectura de credenciales desde fichero 'cuentas.cfg'
+# *********************************************************
+
+    # Abrimos el fichero que contiene las credenciales de información estática
+ficherocs = open ('cuentas.cfg','r')
+
+    # inicializamos un nombre para cada lista de variables
+proveedor = []
+direccion = []
+usuario = []
+clave = []
+sucursal = []
+
+    # con este bucle creamos las listas de los diferentes datos
+for i in ficherocs:
+	campos = i.split(';;')
+        proveedor.append(campos[1])
+        direccion.append(campos[2])
+        usuario.append(campos[3])
+        clave.append(campos[4])
+        sucursal.append(campos[5])
+
+    # cerramos el fichero de credenciales
+ficherocs.close()
+
 
 #if "area" not in form or "region" not in form or "ciudad" not in form:
 #    print "<H1>Error</H1>"
@@ -33,13 +62,15 @@ form = cgi.FieldStorage()
 
 URL = 'http://wstest.rhodasol.es/wsserhs/rhodasol'
 
+#===============================================================================
 try:
     urllib.urlopen(URL).read()
     print "Conectado al servidor",'\n'
 except:
     print "No es posible conectar con el servidor"
     sys.exit(1)
-
+ 
+#===============================================================================
 
 # Indicamos el elemento raiz de petición con los atributos 'type',
     # 'version' y 'sessionId' 
@@ -62,7 +93,7 @@ criterio = etree.SubElement(criterio_b,"criterion", \
 criterio = etree.SubElement(criterio_b,"criterion", \
     attrib={"type":"0", "code":"area", "value":""})
 criterio = etree.SubElement(criterio_b,"criterion", \
-    attrib={"type":"0", "code":"region", "value":"41"})
+    attrib={"type":"0", "code":"region", "value":"28"})
 criterio = etree.SubElement(criterio_b,"criterion", \
     attrib={"type":"0", "code":"city", "value":""})
 criterio = etree.SubElement(criterio_b,"criterion", \
@@ -84,20 +115,56 @@ criterio = etree.SubElement(criterio_b,"criterion", \
     # Definimos el cuarto hijo del elemento raiz, es periodo y tiene los 
     # atributos de inicio > start y fin > end
 periodo = etree.SubElement(raizpeticion, "period", \
-    attrib={"start":"20120727", "end":"20120729"})
+    attrib={"start":"20120723", "end":"20120724"})
     # Definimos el quinto hijo del raiz, es rooms contiene los elementos de la estancia
 habitaciones = etree.SubElement(raizpeticion, "rooms")
     # Definimos el primer hijo de rooms
 habitacion = etree.SubElement(habitaciones, "room", \
     attrib={"type":"1", "adults":"2", "children":"0"})
+habitacion = etree.SubElement(habitaciones, "room", \
+    attrib={"type":"2", "adults":"2", "children":"0"})
 
 pet_xml = etree.tostring(arbolpeticion,pretty_print=True)
 
+#pet_xml = open('peticion6.xml','r')
+
+#f = open("peticion.xml","w")
+#f.write(pet_xml)
+#f.close()
 print pet_xml
 
-parameter = urllib.urlencode({'XML': pet_xml})
-response = urllib.urlopen(URL, parameter)
+#parameter = urllib.urlencode({'XML':pet_xml})
+#response = urllib.urlopen(URL, parameter)
+#result = response.read()
+
+opener = urllib2.build_opener()
+opener.addheaders = [('Accept', 'application/xml'),
+                    ('User-Agent', 'Python-urllib/2.6')]
+
+
+req = urllib2.Request(url=URL, data= pet_xml,
+                      headers={'Content-Type': 'application/xml'})
+
+#req = urllib2.Request(url=URL, data=pet_xml)
+assert req.get_method() == 'POST'
+
+response = opener.open(req)
+print response.code
+
+
+
+
+#opener = urllib2.build_opener()
+#opener.addheaders = [('Accept', 'application/xml'),
+#                     ('Content-Type', 'application/xml'),
+#                     ('User-Agent', 'Python-urllib/2.6')]
+#
+#req = urllib2.Request(url=URL, data=pet_xml)
+#assert req.get_method() == 'POST'
+#
+#response = opener.open(req)
+# 
+#respuesta = etree.fromstring(req)
 print response.read()
 
-
-# print etree.tostring(respuesta,pretty_print=True)
+#print etree.tostring(respuesta,pretty_print=True)
